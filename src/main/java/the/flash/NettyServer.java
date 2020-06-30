77a690f1fa187334052579e9eda849dc90951fd8
sunyajun <sunyajun@bilibili.com>
2020-06-30 22:56:27 +0800

    log: add log;

--- src/main/java/the/flash/NettyServer.java
+++ src/main/java/the/flash/NettyServer.java
@@ -19,13 +19,20 @@ public class NettyServer {
         final ServerBootstrap serverBootstrap = new ServerBootstrap();
         final AttributeKey<Object> clientKey = AttributeKey.newInstance("clientKey");
         serverBootstrap
+                // bossGroup表示监听端口，accept 新连接的线程组，workerGroup表示处理每一条连接的数据读写的线程组
                 .group(boosGroup, workerGroup)
+                // 指定我们服务端的 IO 模型为NIO; 想指定 IO 模型为 BIO，那么这里配置上OioServerSocketChannel.class
                 .channel(NioServerSocketChannel.class)
+                // NioServerSocketChannel指定一些自定义属性，然后我们可以通过channel.attr()取出这个属性
                 .attr(AttributeKey.newInstance("serverName"), "nettyServer")
+                // childAttr可以给每一条连接指定自定义属性
                 .childAttr(clientKey, "clientValue")
+                // 给服务端channel设置一些属性
                 .option(ChannelOption.SO_BACKLOG, 1024)
+                // 给每条连接设置一些TCP底层相关的属性
                 .childOption(ChannelOption.SO_KEEPALIVE, true)
                 .childOption(ChannelOption.TCP_NODELAY, true)
+                // 定义后续每条连接的数据读写，NioSocketChannelNetty 对 NIO 类型的连接的抽象
                 .childHandler(new ChannelInitializer<NioSocketChannel>() {
                     protected void initChannel(NioSocketChannel ch) {
                         System.out.println(ch.attr(clientKey).get());
